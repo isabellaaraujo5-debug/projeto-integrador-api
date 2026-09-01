e7c519739dce01a0b2d5c99a02f231db6cae36ce
Isabella Cristina L de Araujo <isabella.araujo5@estudante.ifto.edu.br>
2026-09-01 15:07:57 -0300

    feat: Adiciona o campo de sinopse

--- filmes/models.py
+++ filmes/models.py
@@ -9,6 +9,7 @@ class Filme(models.Model):
    titulo = models.CharField(max_length=150)
    ano = models.IntegerField(default=0)
    diretor = models.CharField(max_length=100, default="Desconhecido") # Novo campo!
+   sinopse = models.TextField(default="")
    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
    def __str__(self):
        return self.titulo
